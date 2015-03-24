a5dd526cccb78b0ea26f03b3c6d4964e1bb0b549
Michel Weststrate <mweststrate@gmail.com>
2015-03-24 22:25:26 +0100

    Initial value is optional now (updated typings)

--- mobservable.d.ts
+++ mobservable.d.ts
@@ -7,12 +7,10 @@ declare module "mobservable" {
 	    (value: T): S;
 	    subscribe(callback: (newValue: T, oldValue: T) => void): Lambda;
 	}
-	function property<T, S>(value: T | {
-	    (): T;
-	}, scope?: S): IProperty<T, S>;
+	function property<T, S>(value?: T | {(): T;}, scope?: S): IProperty<T, S>;
 	function guard<T>(func: () => T, onInvalidate: Lambda): [T, Lambda];
 	function batch(action: Lambda): void;
 	function onReady(listener: Lambda): Lambda;
 	function onceReady(listener: Lambda): void;
-	function defineProperty<T>(object: Object, name: string, initialValue: T): void;
+	function defineProperty<T>(object: Object, name: string, initialValue?: T): void;
 }
