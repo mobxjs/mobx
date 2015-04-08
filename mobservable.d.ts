6fa22399f6e233b61924d458f69ef1e7495a7e7f
Michel Weststrate <michel.weststrate@mendix.com>
2015-04-08 08:39:20 +0200

    mobservable function is not decently required when exported as variables, so work around that with an verbose import statement: 'var mobservable:MobservableStatic = require('mobservable');' :'(

--- mobservable.d.ts
+++ mobservable.d.ts
@@ -1,35 +1,35 @@
+interface Lambda {
+	(): void;
+}
+interface IObservableValue<T, S> {
+	(): T;
+	(value: T): S;
+	subscribe(callback: (newValue: T, oldValue: T) => void): Lambda;
+}
+
+interface MobservableStatic {
+	<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
+
+	value<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
+	watch<T>(func:()=>T, onInvalidate:Lambda):[T,Lambda];
+	array<T>(values?:T[]): IObservableArray<T>;
+	batch(action:Lambda);
+	onReady(listener:Lambda):Lambda;
+	onceReady(listener:Lambda);
+	defineProperty<T>(object:Object, name:string, initialValue?:T);
+}
+
+ interface IObservableArray<T> extends Array<T> {
+	[n: number]: T;
+	length: number;
+
+	spliceWithArray(index:number, deleteCount?:number, newItems?:T[]):T[];
+	observe(listener:()=>void, fireImmediately:boolean):Lambda;
+	clear(): T[];
+	replace(newItems:T[]);
+	values(): T[];
+}
+
 declare module "mobservable" {
-	interface Lambda {
-		(): void;
-	}
-	interface IObservableValue<T, S> {
-		(): T;
-		(value: T): S;
-		subscribe(callback: (newValue: T, oldValue: T) => void): Lambda;
-	}
-
-	interface MobservableStatic {
-		<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
-
-		value<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
-		watch<T>(func:()=>T, onInvalidate:Lambda):[T,Lambda];
-		array<T>(values?:T[]): IObservableArray<T>;
-		batch(action:Lambda);
-		onReady(listener:Lambda):Lambda;
-		onceReady(listener:Lambda);
-		defineProperty<T>(object:Object, name:string, initialValue?:T);
-	}
-
-	 interface IObservableArray<T> extends Array<T> {
-		[n: number]: T;
-		length: number;
-
-		spliceWithArray(index:number, deleteCount?:number, newItems?:T[]):T[];
-		observe(listener:()=>void, fireImmediately:boolean):Lambda;
-		clear(): T[];
-		replace(newItems:T[]);
-		values(): T[];
-	}
-
 	export = MobservableStatic;
 }
