558df55cc8e8dbd286a71db4b2887d79f57dbba5
Michel Weststrate <michel.weststrate@mendix.com>
2015-04-08 08:49:10 +0200

    no global export function in favor of clean typescript imports...

--- mobservable.d.ts
+++ mobservable.d.ts
@@ -1,35 +1,29 @@
-interface Lambda {
-	(): void;
-}
-interface IObservableValue<T, S> {
-	(): T;
-	(value: T): S;
-	subscribe(callback: (newValue: T, oldValue: T) => void): Lambda;
-}
-
-interface MobservableStatic {
-	<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
-
-	value<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
-	watch<T>(func:()=>T, onInvalidate:Lambda):[T,Lambda];
-	array<T>(values?:T[]): IObservableArray<T>;
-	batch(action:Lambda);
-	onReady(listener:Lambda):Lambda;
-	onceReady(listener:Lambda);
-	defineProperty<T>(object:Object, name:string, initialValue?:T);
-}
-
- interface IObservableArray<T> extends Array<T> {
-	[n: number]: T;
-	length: number;
-
-	spliceWithArray(index:number, deleteCount?:number, newItems?:T[]):T[];
-	observe(listener:()=>void, fireImmediately:boolean):Lambda;
-	clear(): T[];
-	replace(newItems:T[]);
-	values(): T[];
-}
-
 declare module "mobservable" {
-	export = MobservableStatic;
+	interface Lambda {
+		(): void;
+	}
+	interface IObservableValue<T, S> {
+		(): T;
+		(value: T): S;
+		subscribe(callback: (newValue: T, oldValue: T) => void): Lambda;
+	}
+
+	export function value<T,S>(value?:T|{():T}, scope?:S):IObservableValue<T,S>;
+	export function watch<T>(func:()=>T, onInvalidate:Lambda):[T,Lambda];
+	export function array<T>(values?:T[]): IObservableArray<T>;
+	export function batch(action:Lambda);
+	export function onReady(listener:Lambda):Lambda;
+	export function onceReady(listener:Lambda);
+	export function defineProperty<T>(object:Object, name:string, initialValue?:T);
+
+	interface IObservableArray<T> extends Array<T> {
+		[n: number]: T;
+		length: number;
+
+		spliceWithArray(index:number, deleteCount?:number, newItems?:T[]):T[];
+		observe(listener:()=>void, fireImmediately:boolean):Lambda;
+		clear(): T[];
+		replace(newItems:T[]);
+		values(): T[];
+	}
 }
