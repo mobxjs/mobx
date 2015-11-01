5d8a7dd3866264ade18781d80557b61c04e159ce
tetsuo <ogunduz@gmail.com>
2015-11-01 08:22:36 +0300

    update typescript test for tape-compat

---
+++ test/typescript/require.d.ts
@@ -0,0 +1,12 @@
+
+/**
+ * From DefinitelyTyped's `node.d.ts` file.
+ */
+
+declare var require: {
+    (id: string): any;
+    resolve(id:string): string;
+    cache: any;
+    extensions: any;
+    main: any;
+};
