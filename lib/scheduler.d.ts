6daff4df57a2a4befa406470a378786d8580ec75
tetsuo <ogunduz@gmail.com>
2015-11-01 08:51:13 +0300

    cleanup

--- lib/scheduler.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * mobservable
- * (c) 2015 - Michel Weststrate
- * https://github.com/mweststrate/mobservable
- */
-import { Lambda } from './interfaces';
-export declare function schedule(func: Lambda): void;
-export declare function transaction<T>(action: () => T): T;
