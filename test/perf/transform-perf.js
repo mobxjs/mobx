ab12dab3b048ec42c07d49845cce5d3ef9a4aee8
Michel Weststrate <mweststrate@gmail.com>
2015-12-28 08:49:41 +0100

    Optimized performance baseline a bit.
    Still signifcant slower

--- test/perf/transform-perf.js
+++ test/perf/transform-perf.js
@@ -4,28 +4,9 @@ var test = require('tape');
 /**
  * This file compares creating a project tree view using a reactive graph transformation 
  * against the same in a plain js implementation (the baseline).
- * Surely the plain version could be optimized further, but the goal here is to compare
- * a version that is similar in complexity:
- * 
-# non-reactive folder tree
-[0ms] create folders
-[1ms] create displayfolders
-[248ms] create text
-[220ms] collapse folder
-[234ms] uncollapse folder
-[241ms] change name of folder
-[56ms] search
-[234ms] unsearch
-
-# reactive folder tree
-[19ms] create folders
-[11ms] create displayfolders
-[91ms] create text
-[5ms] collapse folder
-[10ms] uncollapse folder
-[25ms] change name of folder
-[27ms] search
-[96ms] unsearch
+ * Surely the plain version could be optimized further,
+ * but even in this comparison the plain version is already significantly more complex than the reactive version,
+ * yet twice as slow.
  */
 
 function measure(title, func) {
@@ -88,13 +69,18 @@ test('non-reactive folder tree', function(t) {
 	};
 	
 	// returns list of strings per folder
-	var stringTransformer = function (displayFolder) {
-		var path = displayFolder.path;
-		return path + "\n" +
-			displayFolder.children.filter(function(child) {
+	DisplayFolder.prototype.rebuild = function () {
+		var path = this.path;
+		this.asText = path + "\n" +
+			this.children.filter(function(child) {
 				return child.isVisible;
-			}).map(stringTransformer).join('');
+			}).map(child => child.asText).join('');
 	};
+	
+	DisplayFolder.prototype.rebuildAll = function() {
+		this.children.forEach(child => child.rebuildAll());
+		this.rebuild();
+	}
 	
 	function createFolders(parent, recursion) {
 		if (recursion === 0)
@@ -106,50 +92,59 @@ test('non-reactive folder tree', function(t) {
 		}
 	}
 	
-	
-	measure("create folders", () => createFolders(state.root, 3)); // 10^3
-	measure("create displayfolders", () => state.displayRoot = transformFolder(state.root));
-	measure("create text", () => {
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[0], "root");
-		t.equal(state.text[state.text.length - 2], "root/9/9/9");
-	});
-	
-	measure("collapse folder", () => {
-		state.displayRoot.children[9].collapsed = true;
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.equal(state.text.length, 1002);
-		t.equal(state.text[state.text.length - 3], "root/8/9/9");
-		t.equal(state.text[state.text.length - 2], "root/9");
-	});
-	
-	measure("uncollapse folder", () => {
-		state.displayRoot.children[9].collapsed = false;
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[state.text.length - 2], "root/9/9/9");
-	});
-	
-	measure("change name of folder", () => {
-		state.root.name = "wow";
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[state.text.length - 2], "wow/9/9/9");
-	});
-	
-	measure("search", () => {
-		state.filter = "8";
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.deepEqual(state.text.slice(0, 4), [ 'wow', 'wow/0', 'wow/0/0', 'wow/0/0/8' ]);
-		t.deepEqual(state.text.slice(-5), [ 'wow/9/8', 'wow/9/8/8', 'wow/9/9', 'wow/9/9/8', '' ]);
-		t.equal(state.text.length, 212);
-	});
-	
-	measure("unsearch", () => {
-		state.filter = null;
-		state.text = stringTransformer(state.displayRoot).split("\n");
-		t.equal(state.text.length, 1112);
+	measure("reactive folder tree [total]\n", () => {
+		measure("create folders", () => createFolders(state.root, 3)); // 10^3
+		measure("create displayfolders", () => state.displayRoot = transformFolder(state.root));
+		measure("create text", () => {
+			state.displayRoot.rebuildAll();
+			state.text = state.displayRoot.asText.split("\n");
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[0], "root");
+			t.equal(state.text[state.text.length - 2], "root/9/9/9");
+		});
+		
+		measure("collapse folder", () => {
+			state.displayRoot.children[9].collapsed = true;
+			state.displayRoot.children[9].rebuild();
+			state.displayRoot.rebuild();
+			state.text = state.displayRoot.asText.split("\n");
+			t.equal(state.text.length, 1002);
+			t.equal(state.text[state.text.length - 3], "root/8/9/9");
+			t.equal(state.text[state.text.length - 2], "root/9");
+		});
+		
+		measure("uncollapse folder", () => {
+			state.displayRoot.children[9].collapsed = false;
+			state.displayRoot.children[9].rebuild();
+			state.displayRoot.rebuild();
+			state.text = state.displayRoot.asText.split("\n");
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[state.text.length - 2], "root/9/9/9");
+		});
+		
+		measure("change name of folder", () => {
+			state.root.name = "wow";
+			state.displayRoot.rebuildAll();
+			state.text = state.displayRoot.asText.split("\n");
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[state.text.length - 2], "wow/9/9/9");
+		});
+		
+		measure("search", () => {
+			state.filter = "8";
+			state.displayRoot.rebuildAll();
+			state.text = state.displayRoot.asText.split("\n");
+			t.deepEqual(state.text.slice(0, 4), [ 'wow', 'wow/0', 'wow/0/0', 'wow/0/0/8' ]);
+			t.deepEqual(state.text.slice(-5), [ 'wow/9/8', 'wow/9/8/8', 'wow/9/9', 'wow/9/9/8', '' ]);
+			t.equal(state.text.length, 212);
+		});
+		
+		measure("unsearch", () => {
+			state.filter = null;
+			state.displayRoot.rebuildAll();
+			state.text = state.displayRoot.asText.split("\n");
+			t.equal(state.text.length, 1112);
+		});
 	});
 	
 	t.end(); 
@@ -217,47 +212,48 @@ test('reactive folder tree', function(t) {
 		}
 	}
 	
-	
-	measure("create folders", () => createFolders(state.root, 3)); // 10^3
-	measure("create displayfolders", () => state.displayRoot = transformFolder(state.root));
-	measure("create text", () => {
-		m.autorun(function() {
-			state.text = stringTransformer(state.displayRoot).split("\n");
+	measure("reactive folder tree [total]\n", () => {
+		measure("create folders", () => createFolders(state.root, 3)); // 10^3
+		measure("create displayfolders", () => state.displayRoot = transformFolder(state.root));
+		measure("create text", () => {
+			m.autorun(function() {
+				state.text = stringTransformer(state.displayRoot).split("\n");
+			});
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[0], "root");
+			t.equal(state.text[state.text.length - 2], "root/9/9/9");
 		});
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[0], "root");
-		t.equal(state.text[state.text.length - 2], "root/9/9/9");
-	});
-	
-	measure("collapse folder", () => {
-		state.displayRoot.children[9].collapsed = true;
-		t.equal(state.text.length, 1002);
-		t.equal(state.text[state.text.length - 3], "root/8/9/9");
-		t.equal(state.text[state.text.length - 2], "root/9");
-	});
-	
-	measure("uncollapse folder", () => {
-		state.displayRoot.children[9].collapsed = false;
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[state.text.length - 2], "root/9/9/9");
-	});
-	
-	measure("change name of folder", () => {
-		state.root.name = "wow";
-		t.equal(state.text.length, 1112);
-		t.equal(state.text[state.text.length - 2], "wow/9/9/9");
-	});
-	
-	measure("search", () => {
-		state.filter = "8";
-		t.deepEqual(state.text.slice(0, 4), [ 'wow', 'wow/0', 'wow/0/0', 'wow/0/0/8' ]);
-		t.deepEqual(state.text.slice(-5), [ 'wow/9/8', 'wow/9/8/8', 'wow/9/9', 'wow/9/9/8', '' ]);
-		t.equal(state.text.length, 212);
-	});
-	
-	measure("unsearch", () => {
-		state.filter = null;
-		t.equal(state.text.length, 1112);
+		
+		measure("collapse folder", () => {
+			state.displayRoot.children[9].collapsed = true;
+			t.equal(state.text.length, 1002);
+			t.equal(state.text[state.text.length - 3], "root/8/9/9");
+			t.equal(state.text[state.text.length - 2], "root/9");
+		});
+		
+		measure("uncollapse folder", () => {
+			state.displayRoot.children[9].collapsed = false;
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[state.text.length - 2], "root/9/9/9");
+		});
+		
+		measure("change name of folder", () => {
+			state.root.name = "wow";
+			t.equal(state.text.length, 1112);
+			t.equal(state.text[state.text.length - 2], "wow/9/9/9");
+		});
+		
+		measure("search", () => {
+			state.filter = "8";
+			t.deepEqual(state.text.slice(0, 4), [ 'wow', 'wow/0', 'wow/0/0', 'wow/0/0/8' ]);
+			t.deepEqual(state.text.slice(-5), [ 'wow/9/8', 'wow/9/8/8', 'wow/9/9', 'wow/9/9/8', '' ]);
+			t.equal(state.text.length, 212);
+		});
+		
+		measure("unsearch", () => {
+			state.filter = null;
+			t.equal(state.text.length, 1112);
+		});
 	});
 	
 	t.end(); 
