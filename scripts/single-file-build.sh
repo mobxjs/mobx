28438fb6a595ff8ba8820a5b845779cc642a743a
Michel Weststrate <mweststrate@gmail.com>
2016-02-20 02:45:32 +0100

    Use browserify for correct umd build

--- scripts/single-file-build.sh
+++ scripts/single-file-build.sh
@@ -6,7 +6,7 @@
 # This makes the library a factor 2 - 3 small, both minified and unminified, because
 # 1) when having many source files, there are a lot of webpack require calls lingering around
 # 2) when export functions from typescript files, they cannot be minified anymore because they are exported 
-#    (or added as prop to a namespace if using namespace),
+#    (or added as prop to a namespace if using typescript namespaces),
 #    while actually they can be minified as long as they are internal to the module 
 
 # prelude
@@ -22,10 +22,11 @@ cat src/mobservable.ts | grep -v '^import' | sed -e 's/from.*$//g' >> .build/mob
 # find all ts files, concat them (with newlines), remove all import statements, remove export keyword
 ls  src/{core,types,api,utils}/*.ts | xargs awk 'BEGINFILE {print "/* file:", FILENAME, "*/"} {print $0}' | grep -v '^import ' | sed -e 's/^export //g' >> .build/mobservable.ts
 
-# compile umd and commonjs, generate declaration, no comments
-tsc -m umd      -t es5 -d --removeComments --outDir lib .build/mobservable.ts 
-mv lib/mobservable.js lib/mobservable.umd.js
+# compile to commonjs, generate declaration, no comments
 tsc -m commonjs -t es5 -d --removeComments --outDir lib .build/mobservable.ts 
+
+# make an umd build as well
+browserify -s mobservable -e lib/mobservable.js -o lib/mobservable.umd.js 
 
 # idea: strip invariants from compiled result. However, difference is not really significant in speed and size, disabled for now. 
 # cat lib/mobservable.js | grep -v -P '^\s+invariant' > .build/mobservable-prod.js 
