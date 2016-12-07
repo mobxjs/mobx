b6df37cce8cb7fe20abb59e57986c3726b29b9d1
Michel Weststrate <mweststrate@gmail.com>
2016-12-07 08:49:36 +0100

    Added missing return types to when / reaction / autorun

--- src/api/autorun.ts
+++ src/api/autorun.ts
@@ -11,7 +11,7 @@ import {action, isAction} from "../api/action";
  * @param scope (optional)
  * @returns disposer function, which can be used to stop the view from being updated in the future.
  */
-export function autorun(view: (r: IReactionPublic) => void, scope?: any);
+export function autorun(view: (r: IReactionPublic) => void, scope?: any): Lambda;
 
 /**
  * Creates a named reactive view and keeps it alive, so that the view is always
@@ -21,7 +21,7 @@ export function autorun(view: (r: IReactionPublic) => void, scope?: any);
  * @param scope (optional)
  * @returns disposer function, which can be used to stop the view from being updated in the future.
  */
-export function autorun(name: string, view: (r: IReactionPublic) => void, scope?: any);
+export function autorun(name: string, view: (r: IReactionPublic) => void, scope?: any): Lambda;
 
 export function autorun(arg1: any, arg2: any, arg3?: any) {
 	let name: string, view: (r: IReactionPublic) => void, scope: any;
@@ -66,7 +66,7 @@ export function autorun(arg1: any, arg2: any, arg3?: any) {
  * @param scope (optional)
  * @returns disposer function to prematurely end the observer.
  */
-export function when(name: string, predicate: () => boolean, effect: Lambda, scope?: any);
+export function when(name: string, predicate: () => boolean, effect: Lambda, scope?: any): Lambda;
 
 /**
  * Similar to 'observer', observes the given predicate until it returns true.
@@ -76,7 +76,7 @@ export function when(name: string, predicate: () => boolean, effect: Lambda, sco
  * @param scope (optional)
  * @returns disposer function to prematurely end the observer.
  */
-export function when(predicate: () => boolean, effect: Lambda, scope?: any);
+export function when(predicate: () => boolean, effect: Lambda, scope?: any): Lambda;
 
 export function when(arg1: any, arg2: any, arg3?: any, arg4?: any) {
 	let name: string, predicate: () => boolean, effect: Lambda, scope: any;
@@ -103,14 +103,14 @@ export function when(arg1: any, arg2: any, arg3?: any, arg4?: any) {
 	return disposer;
 }
 
-export function autorunUntil(predicate: () => boolean, effect: (r: IReactionPublic) => void, scope?: any) {
+export function autorunUntil(predicate: () => boolean, effect: (r: IReactionPublic) => void, scope?: any): Lambda {
 	deprecated("`autorunUntil` is deprecated, please use `when`.");
 	return when.apply(null, arguments);
 }
 
-export function autorunAsync(name: string, func: (r: IReactionPublic) => void, delay?: number, scope?: any);
+export function autorunAsync(name: string, func: (r: IReactionPublic) => void, delay?: number, scope?: any): Lambda;
 
-export function autorunAsync(func: (r: IReactionPublic) => void, delay?: number, scope?: any);
+export function autorunAsync(func: (r: IReactionPublic) => void, delay?: number, scope?: any): Lambda;
 
 export function autorunAsync(arg1: any, arg2: any, arg3?: any, arg4?: any) {
 	let name: string, func: (r: IReactionPublic) => void, delay: number, scope: any;
@@ -160,7 +160,7 @@ export function autorunAsync(arg1: any, arg2: any, arg3?: any, arg4?: any) {
  * or
  * autorun(() => action(effect)(expr));
  */
-export function reaction<T>(name: string, expression: () => T, effect: (arg: T, r: IReactionPublic) => void, fireImmediately?: boolean, delay?: number, scope?: any);
+export function reaction<T>(name: string, expression: () => T, effect: (arg: T, r: IReactionPublic) => void, fireImmediately?: boolean, delay?: number, scope?: any): Lambda;
 
 /**
  *
@@ -168,7 +168,7 @@ export function reaction<T>(name: string, expression: () => T, effect: (arg: T, 
  * or
  * autorun(() => action(effect)(expr));
  */
-export function reaction<T>(expression: () => T, effect: (arg: T, r: IReactionPublic) => void, fireImmediately?: boolean, delay?: number, scope?: any);
+export function reaction<T>(expression: () => T, effect: (arg: T, r: IReactionPublic) => void, fireImmediately?: boolean, delay?: number, scope?: any): Lambda;
 
 export function reaction<T>(arg1: any, arg2: any, arg3: any, arg4?: any, arg5?: any, arg6?: any) {
 	let name: string, expression: () => T, effect: (arg: T, r: IReactionPublic) => void, fireImmediately: boolean, delay: number, scope: any;
