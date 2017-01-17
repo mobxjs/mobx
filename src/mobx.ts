b5a0ea2a3cc710a49417312323142172514adde6
Dor Tchizik <dor@tipranks.com>
2017-01-17 20:32:27 +0200

    Fix build for Windows. Fix #770
    - Remove .sh script for build.
    - Add .js script for build.
    - Change package.json small-build script to reflect the two aforementioned changes.
    
    Cheers love! #GoodnessSquad

--- src/mobx.ts
+++ src/mobx.ts
@@ -35,7 +35,7 @@ export { IObjectWillChange, IObjectChange, IObservableObject, isObservableObject
 
 export { IValueDidChange, IValueWillChange, IObservableValue } from "./types/observablevalue";
 export { IObservableArray, IArrayWillChange, IArrayWillSplice, IArrayChange, IArraySplice, isObservableArray } from "./types/observablearray";
-export { IKeyValueMap, ObservableMap, IMapEntries, IMapEntry, IMapWillChange, IMapChange, isObservableMap, map, IObservableMapInitialValues, IMap } from "./types/observablemap"
+export { IKeyValueMap, ObservableMap, IMapEntries, IMapEntry, IMapWillChange, IMapChange, isObservableMap, map, IObservableMapInitialValues, IMap } from "./types/observablemap";
 
 export { transaction                                          } from "./api/transaction";
 export { observable, IObservableFactory, IObservableFactories } from "./api/observable";
