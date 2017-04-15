4348fe1c5e536b9b880883ca3e5846b1b75c698c
jamiewinder <jamiewinder@gmail.com>
2017-04-15 15:32:58 +0100

    add comparers

---
+++ src/types/comparer.ts
@@ -0,0 +1,13 @@
+import { deepEqual } from '../utils/utils';
+
+export type EqualityComparer<T> = (a: T, b: T) => boolean;
+
+export const equalityComparer = (a: any, b: any) => a == b;
+export const identityComparer = (a: any, b: any) => a === b;
+export const structuralComparer = (a: any, b: any) => deepEqual(a, b);
+export const defaultComparer = (a: any, b: any) => {
+    if (typeof a === 'number' && typeof b === 'number' && isNaN(a) && isNaN(b)) {
+        return true;
+    }
+    return identityComparer(a, b);
+};
