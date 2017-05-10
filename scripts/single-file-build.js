acb8adc2062fc8b4e580de1ff2880f3396c9fe22
Sören Oldag <soeren_oldag@freenet.de>
2017-05-10 10:10:48 +0200

    single-file-build now can handle spaces in path.

--- scripts/single-file-build.js
+++ scripts/single-file-build.js
@@ -43,10 +43,11 @@ fs.writeFileSync('.build/mobx.ts', allContents, { encoding: 'utf8', flag: 'a' })
     `uglifyjs -m sort,toplevel -c --screw-ie8 --preamble "/** MobX - (c) Michel Weststrate 2015, 2016 - MIT Licensed */" --source-map lib/mobx.umd.min.js.map -o lib/mobx.umd.min.js lib/mobx.umd.js`,
     `ncp flow-typed/mobx.js lib/mobx.js.flow`
 ]
-    .map(cmd => `${__dirname}/../node_modules/.bin/${cmd}`)
+    .map(cmd => `${__dirname.replace(/ /g, '\\ ')}/../node_modules/.bin/${cmd}`)
     .map(cmd => {
 		try {
-			exec(cmd);
+      console.log(cmd);
+      exec(cmd);
 		} catch (e) {
 			console.log(e.stdout.toString());
 			console.error(e.stderr.toString());
