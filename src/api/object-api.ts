7ab27b229e8b48cdb838929f56c89c49e87fee05
Michel Weststrate <mweststrate@gmail.com>
2018-03-02 09:00:16 +0100

    Improved typings of object api

--- src/api/object-api.ts
+++ src/api/object-api.ts
@@ -1,7 +1,6 @@
 import { isObservableMap, ObservableMap } from "../types/observablemap"
 import {
     isObservableObject,
-    IObservableObject,
     IIsObservableObject,
     defineObservableProperty
 } from "../types/observableobject"
@@ -10,8 +9,8 @@ import { fail, invariant } from "../utils/utils"
 import { deepEnhancer } from "../types/modifiers"
 import { startBatch, endBatch } from "../core/observable"
 
-export function keys(obj: IObservableObject): ReadonlyArray<string>
 export function keys<K>(map: ObservableMap<K, any>): ReadonlyArray<K>
+export function keys<T extends Object>(obj: T): ReadonlyArray<string>
 export function keys(obj: any): any {
     if (isObservableObject(obj)) {
         return ((obj as any) as IIsObservableObject).$mobx.getKeys()
@@ -25,9 +24,9 @@ export function keys(obj: any): any {
     )
 }
 
-export function values<T = any>(obj: IObservableObject): ReadonlyArray<any>
 export function values<K, T>(map: ObservableMap<K, T>): ReadonlyArray<T>
 export function values<T>(ar: IObservableArray<T>): ReadonlyArray<T>
+export function values<T = any>(obj: T): ReadonlyArray<any>
 export function values(obj: any): string[] {
     if (isObservableObject(obj)) {
         return keys(obj).map(key => obj[key])
@@ -45,10 +44,10 @@ export function values(obj: any): string[] {
 }
 
 export function set<V>(obj: ObservableMap<string, V>, values: { [key: string]: V })
-export function set(obj: IObservableObject, values: { [key: string]: any })
-export function set<T>(obj: IObservableArray<T>, index: number, value: T)
 export function set<K, V>(obj: ObservableMap<K, V>, key: K, value: V)
-export function set(obj: IObservableObject, key: string, value: any)
+export function set<T>(obj: IObservableArray<T>, index: number, value: T)
+export function set<T extends Object>(obj: T, values: { [key: string]: any })
+export function set<T extends Object>(obj: T, key: string, value: any)
 export function set(obj: any, key: any, value?: any): void {
     if (arguments.length === 2) {
         startBatch()
@@ -86,9 +85,9 @@ export function set(obj: any, key: any, value?: any): void {
     }
 }
 
-export function remove(obj: IObservableObject, key: string)
 export function remove<K, V>(obj: ObservableMap<K, V>, key: K)
 export function remove<T>(obj: IObservableArray<T>, index: number)
+export function remove<T extends Object>(obj: T, key: string)
 export function remove(obj: any, key: any): void {
     if (isObservableObject(obj)) {
         const adm = ((obj as any) as IIsObservableObject).$mobx
