86f32d36e7339daafbc0b01d681dd74f2b93f4c1
Michel Weststrate <mweststrate@gmail.com>
2018-08-27 10:41:06 +0200

    Warn about the absence of Symbols

--- src/mobx.ts
+++ src/mobx.ts
@@ -16,9 +16,9 @@
  *
  */
 
-if (typeof Proxy === "undefined") {
+if (typeof Proxy === "undefined" || typeof Symbol === "undefined") {
     throw new Error(
-        "[mobx] MobX 5+ requires Proxy objects. If your environment doesn't support Proxy objects, please downgrade to MobX 4."
+        "[mobx] MobX 5+ requires Proxy and Symbol objects. If your environment doesn't support Proxy objects, please downgrade to MobX 4. For React Native Android, consider upgrading JSCore."
     )
 }
 
