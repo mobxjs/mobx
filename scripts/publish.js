ff78f69dbef40912b799ce638e87ee3c9dd3c05e
Javier Gonzalez <xaviergonz@users.noreply.github.com>
2018-09-27 15:08:57 +0200

    Fix windows compilation + remove nscript due to fibers dep (#1735)
    * remove nscript due to the node-gyp dependency
    
    * added publish script to package json
    
    * fix build on windows
    
    * fix for question
    
    * another fix for publish.js
    
    * small tweak to package.json
    
    * restoring format to package json
    
    * restored formatting to package.json, added it to prettierignore

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,41 +1,86 @@
-#!node_modules/.bin/nscript
-/* To run this script, nscript is needed: [sudo] npm install -g nscript
 /* Publish.js, publish a new version of the npm package as found in the current directory */
 /* Run this file from the root of the repository */
-module.exports = function(shell, npm, git) {
-    // build
-    npm("run", "small-build")
 
-    var pkg = JSON.parse(shell.read("package.json"))
+const shell = require("shelljs")
+const fs = require("fs")
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function run(command, options) {
+    const continueOnErrors = options && options.continueOnErrors
+    const ret = shell.exec(command, options)
+    if (!continueOnErrors && ret.code !== 0) {
+        shell.exit(1)
+    }
+    return ret
+}
+
+function exit(code, msg) {
+    console.error(msg)
+    shell.exit(code)
+}
+
+async function prompt(question, defaultValue) {
+    return new Promise(resolve => {
+        rl.question(`${question} [${defaultValue}]: `, answer => {
+            answer = answer && answer.trim()
+            resolve(answer ? answer : defaultValue)
+        })
+    })
+}
+
+async function main() {
+    // build
+    run("npm run small-build")
+
+    const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"))
 
     // Bump version number
-    var nrs = pkg.version.split(".")
+    let nrs = pkg.version.split(".")
     nrs[2] = 1 + parseInt(nrs[2], 10)
-    var version = (pkg.version = shell.prompt(
+    const version = (pkg.version = await prompt(
         "Please specify the new package version of '" + pkg.name + "' (Ctrl^C to abort)",
         nrs.join(".")
     ))
-    if (!version.match(/^\d+\.\d+\.\d+$/)) shell.exit(1, "Invalid semantic version: " + version)
+    if (!version.match(/^\d+\.\d+\.\d+$/)) {
+        exit(1, "Invalid semantic version: " + version)
+    }
 
-    // Check registery data
-    if (npm.silent().test("info", pkg.name)) {
+    // Check registry data
+    const npmInfoRet = run(`npm info ${pkg.name} --json`, {
+        continueOnErrors: true,
+        silent: true
+    })
+    if (npmInfoRet.code === 0) {
         //package is registered in npm?
-        var publishedPackageInfo = JSON.parse(npm.get("info", pkg.name, "--json"))
+        var publishedPackageInfo = JSON.parse(npmInfoRet.stdout)
         if (
             publishedPackageInfo.versions == version ||
-            publishedPackageInfo.versions.indexOf(version) != -1
-        )
-            shell.exit(2, "Version " + pkg.version + " is already published to npm")
+            publishedPackageInfo.versions.includes(version)
+        ) {
+            exit(2, "Version " + pkg.version + " is already published to npm")
+        }
 
-        shell.write("package.json", JSON.stringify(pkg, null, 2))
+        fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2), "utf8")
 
         // Finally, commit and publish!
-        npm("publish")
-        git("commit", "-am", "Published version " + version)
-        git("tag", version)
+        run("npm publish")
+        run(`git commit -am "Published version ${version}"`)
+        run(`git tag ${version}`)
 
-        git("push")
-        git("push", "--tags")
+        run("git push")
+        run("git push --tags")
         console.log("Published!")
-    } else shell.exit(1, pkg.name + " is not an existing npm package")
+        exit(0)
+    } else {
+        exit(1, pkg.name + " is not an existing npm package")
+    }
 }
+
+main().catch(e => {
+    throw e
+})
