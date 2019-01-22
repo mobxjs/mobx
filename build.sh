73d1227d82726a4556ba3c546daac9bebbc65fd7
Michel Weststrate <mweststrate@gmail.com>
2019-01-22 10:43:27 +0100

    Regenned

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd docs
 # First time: npm install -g gitbook-cli
+# Note: this version of gitbook is known to work under node 6.10.0, but breaks under node 10. Use nvm to switch node version if needed
 gitbook build --gitbook=2.6.9
 find _book -name '*.html' | xargs sed -i -e 's/https:\/\b/https:\/\//' -e 's/http:\/\b/http:\/\//'
 cp -rf _book/* ../
