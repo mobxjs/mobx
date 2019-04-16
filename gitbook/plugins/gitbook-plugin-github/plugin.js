40e0af528def91fcc66bfcb93fb97f8ce4563d13
Michel Weststrate <mweststrate@gmail.com>
2019-04-16 14:44:42 +0200

    Regenned

--- gitbook/plugins/gitbook-plugin-github/plugin.js
+++ gitbook/plugins/gitbook-plugin-github/plugin.js
@@ -1,22 +1,14 @@
 require([ 'gitbook' ], function (gitbook) {
-  var githubURL;
+    gitbook.events.bind('start', function (e, config) {
+        var githubURL = config.github.url;
 
-  function insertGitHubLink() {
-    if (githubURL && jQuery('.github-sharing-link').length === 0) {
-      jQuery('.book-header > h1').before(
-        '<a href="' + githubURL + '" _target="blank" class="btn pull-right github-sharing-link sharing-link" aria-label="GitHub">' +
-          '<i class="fa fa-github"></i>' +
-        '</a>'
-      );
-    }
-  }
-
-  gitbook.events.bind('start', function (e, config) {
-    githubURL = config.github.url;
-    insertGitHubLink();
-  });
-
-  gitbook.events.bind('page.change', function () {
-    insertGitHubLink();
-  });
+        gitbook.toolbar.createButton({
+            icon: 'fa fa-github',
+            label: 'GitHub',
+            position: 'right',
+            onClick: function() {
+                window.open(githubURL)
+            }
+        });
+    });
 });
