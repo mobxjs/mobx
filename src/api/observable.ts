d0faeec8d19b7a02b9cc87430ef384d80b7b41bc
xaviergonz <xaviergonz@gmail.com>
2019-05-29 21:56:15 +0200

    fix observable set ts typing

--- src/api/observable.ts
+++ src/api/observable.ts
@@ -112,6 +112,7 @@ export interface IObservableFactory {
     (value: number | string | null | undefined | boolean): never // Nope, not supported, use box
     (target: Object, key: string | symbol, baseDescriptor?: PropertyDescriptor): any // decorator
     <T = any>(value: T[], options?: CreateObservableOptions): IObservableArray<T>
+    <T = any>(value: Set<T>, options?: CreateObservableOptions): ObservableSet<T>
     <K = any, V = any>(value: Map<K, V>, options?: CreateObservableOptions): ObservableMap<K, V>
     <T extends Object>(
         value: T,
