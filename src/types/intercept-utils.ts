e62cbbed5834fb3aab139dd6672cd633aa1bec66
Felix Gerschau <felix.gerschau@gmail.com>
2019-10-16 16:17:31 +0200

    Add comment

--- src/types/intercept-utils.ts
+++ src/types/intercept-utils.ts
@@ -29,6 +29,7 @@ export function interceptChange<T>(
 ): T | null {
     const prevU = untrackedStart()
     try {
+        // Interceptor can modify the array, copy it to avoid concurrent modification, see #1950
         const interceptors = [...(interceptable.interceptors || [])]
         for (let i = 0, l = interceptors.length; i < l; i++) {
             change = interceptors[i](change)
