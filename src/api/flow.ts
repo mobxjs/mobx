acd28fc3c003e6b02d1ca07596f2f5bf31e8da28
xaviergonz <xaviergonz@gmail.com>
2019-10-17 19:29:02 +0200

    fix for flow typings

--- src/api/flow.ts
+++ src/api/flow.ts
@@ -4,35 +4,9 @@ let generatorId = 0
 
 export type CancellablePromise<T> = Promise<T> & { cancel(): void }
 
-export interface FlowYield {
-    // fake, only for typing
-    "!!flowYield": undefined
-}
-
-export interface FlowReturn<T> {
-    // fake, only for typing
-    "!!flowReturn": T
-}
-
-// we skip promises that are the result of yielding promises (except if they use flowReturn)
-export type FlowReturnType<R> = IfAllAreFlowYieldThenVoid<
-    R extends FlowReturn<infer FR>
-        ? FR extends Promise<infer FRP>
-            ? FRP
-            : FR
-        : R extends Promise<any>
-        ? FlowYield
-        : R
->
-
-// we extract yielded promises from the return type
-export type IfAllAreFlowYieldThenVoid<R> = Exclude<R, FlowYield> extends never
-    ? void
-    : Exclude<R, FlowYield>
-
 export function flow<R, Args extends any[]>(
-    generator: (...args: Args) => IterableIterator<R>
-): (...args: Args) => CancellablePromise<FlowReturnType<R>> {
+    generator: (...args: Args) => Generator<any, R, any>
+): (...args: Args) => CancellablePromise<R> {
     if (arguments.length !== 1)
         fail(
             !!process.env.NODE_ENV && `Flow expects one 1 argument and cannot be used as decorator`
@@ -102,7 +76,7 @@ export function flow<R, Args extends any[]>(
             try {
                 if (pendingPromise) cancelPromise(pendingPromise)
                 // Finally block can return (or yield) stuff..
-                const res = gen.return!()
+                const res = gen.return!(undefined as any)
                 // eat anything that promise would do, it's cancelled!
                 const yieldedPromise = Promise.resolve(res.value)
                 yieldedPromise.then(noop, noop)
@@ -113,7 +87,7 @@ export function flow<R, Args extends any[]>(
                 rejector(e) // there could be a throwing finally block
             }
         })
-        return promise as CancellablePromise<FlowReturnType<R>>
+        return promise as CancellablePromise<R>
     }
 }
 
