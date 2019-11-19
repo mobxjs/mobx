c6c571c78f67897169199e0ce20df50a6066b2a2
kubk <ya.7gorbachev2016@yandex.ru>
2019-11-19 23:30:14 +0300

    Fix type inference of the action callback arguments

--- src/api/action.ts
+++ src/api/action.ts
@@ -11,33 +11,9 @@ import {
 
 export interface IActionFactory {
     // nameless actions
-    <A1, R, T extends (a1: A1) => R>(fn: T): T & IAction
-    <A1, A2, R, T extends (a1: A1, a2: A2) => R>(fn: T): T & IAction
-    <A1, A2, A3, R, T extends (a1: A1, a2: A2, a3: A3) => R>(fn: T): T & IAction
-    <A1, A2, A3, A4, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4) => R>(fn: T): T & IAction
-    <A1, A2, A3, A4, A5, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R>(fn: T): T &
-        IAction
-    <A1, A2, A3, A4, A5, A6, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4, a6: A6) => R>(fn: T): T &
-        IAction
-
+    <T>(fn: T): T & IAction
     // named actions
-    <A1, R, T extends (a1: A1) => R>(name: string, fn: T): T & IAction
-    <A1, A2, R, T extends (a1: A1, a2: A2) => R>(name: string, fn: T): T & IAction
-    <A1, A2, A3, R, T extends (a1: A1, a2: A2, a3: A3) => R>(name: string, fn: T): T & IAction
-    <A1, A2, A3, A4, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4) => R>(name: string, fn: T): T &
-        IAction
-    <A1, A2, A3, A4, A5, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R>(
-        name: string,
-        fn: T
-    ): T & IAction
-    <A1, A2, A3, A4, A5, A6, R, T extends (a1: A1, a2: A2, a3: A3, a4: A4, a6: A6) => R>(
-        name: string,
-        fn: T
-    ): T & IAction
-
-    // generic forms
-    <T extends Function>(fn: T): T & IAction
-    <T extends Function>(name: string, fn: T): T & IAction
+    <T>(name: string, fn: T): T & IAction
 
     // named decorator
     (customName: string): (
