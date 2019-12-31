10042b369ede77acf0deab6dbe4f32f820131d7c
Daniel K <FredyC@users.noreply.github.com>
2019-12-31 11:18:51 +0100

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
                     </a>
                     <div>
                         <h5>Docs</h5>
-                        <a href={this.docUrl("README")}>Why MobX</a>
+                        <a href={this.docUrl("README#introduction")}>Why MobX</a>
                         <a href={this.docUrl("intro/concepts")}>Concepts</a>
                     </div>
                     <div>
