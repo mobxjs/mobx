0330fb80057539928d3c1836852565bb75280b0e
Dave Feucht <dfeucht@lequest.nl>
2020-01-09 11:05:36 +0100

    Updates/removes unit tests which assumed extendObservable constraint

--- test/base/proxies.js
+++ test/base/proxies.js
@@ -20,7 +20,11 @@ test("should react to key removal (unless reconfiguraing to empty) - 1", () => {
         z: 1
     })
 
-    reaction(() => Object.keys(x), keys => events.push(keys.join(",")), { fireImmediately: true })
+    reaction(
+        () => Object.keys(x),
+        keys => events.push(keys.join(",")),
+        { fireImmediately: true }
+    )
     expect(events).toEqual(["y,z"])
     delete x.y
     expect(events).toEqual(["y,z", "z"])
@@ -36,7 +40,10 @@ test("should react to key removal (unless reconfiguraing to empty) - 2", () => {
         z: 1
     })
 
-    reaction(() => x.z, v => events.push(v))
+    reaction(
+        () => x.z,
+        v => events.push(v)
+    )
 
     delete x.z
     expect(events).toEqual([undefined])
@@ -49,7 +56,10 @@ test("should react to key removal (unless reconfiguraing to empty) - 2", () => {
         z: undefined
     })
 
-    reaction(() => x.z, v => events.push(v))
+    reaction(
+        () => x.z,
+        v => events.push(v)
+    )
 
     delete x.z
     expect(events).toEqual([])
@@ -59,7 +69,10 @@ test("should react to future key additions - 1", () => {
     const events = []
     const x = observable.object({})
 
-    reaction(() => Object.keys(x), keys => events.push(keys.join(",")))
+    reaction(
+        () => Object.keys(x),
+        keys => events.push(keys.join(","))
+    )
 
     x.y = undefined
     expect(events).toEqual(["y"])
@@ -88,18 +101,6 @@ test("should react to future key additions - 2", () => {
     expect(events).toEqual([4])
 })
 
-test("should throw clear warning if trying to add computed to already reserved key", () => {
-    const x = observable.object({ z: 3 })
-
-    expect(() => {
-        extendObservable(x, {
-            get z() {
-                return 3
-            }
-        })
-    }).toThrow(/can only be used to introduce new properties/)
-})
-
 test("correct keys are reported", () => {
     const x = observable.object({
         x: 1,
@@ -119,7 +120,11 @@ test("correct keys are reported", () => {
 
     expect(Object.keys(x)).toEqual(["x", "z", "a"])
     expect(Object.values(x)).toEqual([1, 3, 4])
-    expect(Object.entries(x)).toEqual([["x", 1], ["z", 3], ["a", 4]])
+    expect(Object.entries(x)).toEqual([
+        ["x", 1],
+        ["z", 3],
+        ["a", 4]
+    ])
 
     expect(Object.getOwnPropertyNames(x)).toEqual(["x", "y", "z", "a", "b"])
     expect(keys(x)).toEqual(["x", "z", "a"])
