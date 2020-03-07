e266f904e0240570a175886b10a95bda4906ad7b
Michel Weststrate <mweststrate@fb.com>
2020-03-07 07:09:34 +0000

    generate constructor

--- codemod/undecorate.ts
+++ codemod/undecorate.ts
@@ -1,4 +1,13 @@
-import { API, ImportSpecifier, FileInfo, Decorator, ASTPath } from "jscodeshift"
+import {
+    API,
+    ImportSpecifier,
+    FileInfo,
+    Decorator,
+    ASTPath,
+    ClassProperty,
+    Node,
+    ClassDeclaration
+} from "jscodeshift"
 
 const validDecorators = ["action", "observable", "computed"]
 
@@ -6,9 +15,16 @@ export const parser = "babylon"
 
 export default function tranform(fileInfo: FileInfo, api: API, options: any) {
     const j = api.jscodeshift
+
+    const superCall = j.expressionStatement(j.callExpression(j.super(), []))
+    const initializeObservablesCall = j.expressionStatement(
+        j.callExpression(j.identifier("initializeObservables"), [j.thisExpression()])
+    )
+
     const source = j(fileInfo.source)
 
     let changed = false
+    let needsInitializeImport = false
     const decoratorsUsed = new Set<String>()
 
     source.find(j.ImportDeclaration).forEach(im => {
@@ -24,11 +40,30 @@ export default function tranform(fileInfo: FileInfo, api: API, options: any) {
         }
     })
 
-    source.find(j.ClassProperty).replaceWith(property => {
-        // @ts-ignore
-        const decorators: Decorator[] = property.value.decorators
+    source.find(j.ClassDeclaration).forEach(clazzPath => {
+        const clazz = clazzPath.value
+        let needsConstructor = false
+
+        clazz.body.body.forEach(prop => {
+            if (j.ClassProperty.check(prop)) {
+                needsConstructor = needsConstructor || handleProperty(prop as any)
+            }
+        })
+
+        if (needsConstructor) {
+            createConstructor(clazz)
+            needsInitializeImport = true
+        }
+    })
+
+    if (needsInitializeImport) {
+        // TODO: create import for initializeObservables
+    }
+
+    function handleProperty(property: ClassProperty & { decorators: Decorator[] }): boolean {
+        const decorators = property.decorators
         if (decorators.length === 0) {
-            return property
+            return false
         }
         if (decorators.length > 1) {
             console.warn("Found multiple decorators, skipping..")
@@ -37,47 +72,81 @@ export default function tranform(fileInfo: FileInfo, api: API, options: any) {
         const expr = decorator.expression
         if (!j.Identifier.check(expr) || !decoratorsUsed.has(expr.name)) {
             warn(`Found unknown decorator `, property)
-            return property
+            return false
         }
         //@x = y
-        if (!property.value.computed && !property.value.static) {
+        if (!property.computed && !property.static) {
             changed = true
             // decorator.remove()
 
             // only identifier expression atm
             const wrappedExpression = j.Identifier.check(decorator.expression)
-                ? j.callExpression(j.identifier(decorator.expression.name), [property.value.value])
+                ? j.callExpression(j.identifier(decorator.expression.name), [property.value])
                 : null // TODO call expression, property access expression
 
-            property.value.variance
-            const res = j.classProperty(
-                property.value.key,
-                wrappedExpression,
-                property.value.typeAnnotation,
-                property.value.static
-            )
-            res.comments = property.value.comments
-            return res
+            property.decorators.splice(0)
+            property.value = wrappedExpression
+            return true
         }
         // if (decorator.parent)
         // fields
-        // methods
+        // methods (note that super method wrapping doesn't need an initializeObservables!)
         // getters
-        return property
-    })
+        return false
+    }
+
+    function createConstructor(clazz: ClassDeclaration) {
+        const needsSuper = !!clazz.superClass
+        let constructorIndex = clazz.body.body.findIndex(
+            member => j.MethodDefinition.check(member) && member.kind === "constructor"
+        )
+        // create a constructor
+        if (constructorIndex === -1) {
+            if (needsSuper) {
+                warn(
+                    "Generated new constructor for class. But since the class does have a base class, it might be needed to revisit the arguments that are passed to `super()`",
+                    clazz
+                )
+            }
+            const constructorDecl = j.methodDefinition(
+                "constructor",
+                j.identifier("constructor"),
+                j.functionExpression(
+                    null,
+                    [],
+                    j.blockStatement(
+                        needsSuper
+                            ? [superCall, initializeObservablesCall]
+                            : [initializeObservablesCall]
+                    )
+                )
+            )
+
+            const firstMethodIndex = clazz.body.body.findIndex(member =>
+                j.MethodDefinition.check(member)
+            )
+            if (firstMethodIndex === -1) {
+                clazz.body.body.push(constructorDecl)
+            } else {
+                clazz.body.body.splice(firstMethodIndex, 0, constructorDecl)
+            }
+        }
+    }
 
     if (!decoratorsUsed.size) {
         return // no mobx in this file
     }
 
+    // TODO: update import statement
+
     if (changed) {
         return source.toSource()
     }
 
-    function warn(msg, path: ASTPath<any>) {
+    function warn(msg, node: Node) {
         console.warn(
-            `[mobx:undecorate] ${msg} ${fileInfo.path}@${path.value.loc.start.line}:${
-                path.value.loc.start.column
+            `[mobx:undecorate] ${msg} ${fileInfo.path}@${node.loc.start.line}:${
+                node.loc.start.column
             }`
         )
     }
