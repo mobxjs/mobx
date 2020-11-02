f4c229259a72f0497d3f9b6a05af9d9c4280d8b1
Tom Enden <tome@wix.com>
2020-11-02 10:21:48 +0200

    support running in web-worker (#2582)
    * support running in a web-worker
    
    * support web-workers: use v5 implementation

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,4 +1,17 @@
-export function getGlobal(): any {
-    // @ts-ignore
-    return typeof global !== "undefined" ? global : window
+declare const window: any
+declare const self: any
+
+const mockGlobal = {}
+
+export function getGlobal() {
+    if (typeof window !== "undefined") {
+        return window
+    }
+    if (typeof global !== "undefined") {
+        return global
+    }
+    if (typeof self !== "undefined") {
+        return self
+    }
+    return mockGlobal
 }
