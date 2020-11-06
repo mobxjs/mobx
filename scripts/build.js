36ef09b745c6fe671f2758d34459cf93519d3a2b
Daniel K <fredy.c@seznam.cz>
2020-11-06 13:52:26 +0100

    Make build script reusable

--- scripts/build.js
+++ scripts/build.js
@@ -3,12 +3,13 @@ const path = require("path")
 const execa = require("execa")
 
 const stdio = ["ignore", "inherit", "ignore"]
-const cwd = path.resolve("../")
 const opts = { stdio }
+
+const packageName = process.argv[2]
 
 // build to publish needs to do more things so it's slower
 // for the CI run and local testing this is not necessary
-const isPublish = process.argv[2] === "publish"
+const isPublish = process.argv[3] === "publish"
 
 const tempMove = name => fs.move(`dist/${name}`, `temp/${name}`)
 const moveTemp = name => fs.move(`temp/${name}`, `dist/${name}`)
@@ -19,27 +20,23 @@ const run = async () => {
         // and these builds cannot be run in parallel because tsdx doesn't allow to change target dir
         await execa("tsdx", ["build", "--format", "esm", "--env", "development"], opts)
         // tsdx will purge dist folder, so it's necessary to move these
-        await tempMove("mobx.esm.development.js")
-        await tempMove("mobx.esm.development.js.map")
+        await tempMove(`${packageName}.esm.development.js`)
+        await tempMove(`${packageName}.esm.development.js.map`)
 
         await execa("tsdx", ["build", "--format", "esm", "--env", "production"], opts)
-        await tempMove("mobx.esm.production.min.js")
-        await tempMove("mobx.esm.production.min.js.map")
+        await tempMove(`${packageName}.esm.production.min.js`)
+        await tempMove(`${packageName}.esm.production.min.js.map`)
     }
 
-    await execa("tsdx", ["build", "--name", "mobx", "--format", "esm,cjs,umd"], opts)
+    await execa("tsdx", ["build", "--name", packageName, "--format", "esm,cjs,umd"], opts)
 
     if (isPublish) {
-        await moveTemp("mobx.esm.development.js")
-        await moveTemp("mobx.esm.development.js.map")
-        await moveTemp("mobx.esm.production.min.js")
-        await moveTemp("mobx.esm.production.min.js.map")
-
+        // move ESM bundles back to dist folder and remove temp
+        await moveTemp(`${packageName}.esm.development.js`)
+        await moveTemp(`${packageName}.esm.development.js.map`)
+        await moveTemp(`${packageName}.esm.production.min.js`)
+        await moveTemp(`${packageName}.esm.production.min.js.map`)
         await fs.remove("temp")
-
-        await fs.copy("flow-typed/mobx.js", "dist/index.js.flow")
-        await fs.copy("../../README.md", "./README.md")
-        await fs.copy("../../LICENSE", "./LICENSE")
     }
 }
 
