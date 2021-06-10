65adb5527812b6bbe1e117daf36eb5201f1a6cc0
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-06-10 15:53:42 -0400

    Allows the force update to be infinitely caled
    This use case is far-fetched but in some contexts (gaming for example) the number of iterations can go beyond the maximum value supported by javascript and end up being `Infinity` which would cause the force update to stop working
    because `Infinity + 1 === Infinity` is true in javascript

--- packages/mobx-react-lite/src/utils/utils.ts
+++ packages/mobx-react-lite/src/utils/utils.ts
@@ -3,11 +3,9 @@ import { useCallback, useState } from "react"
 const EMPTY_ARRAY: any[] = []
 
 export function useForceUpdate() {
-    const [, setTick] = useState(0)
+    const [, setRef] = useState({})
 
-    const update = useCallback(() => {
-        setTick(tick => tick + 1)
-    }, EMPTY_ARRAY)
+    const update = useCallback(() => setRef(() => ({})), EMPTY_ARRAY)
 
     return update
 }
