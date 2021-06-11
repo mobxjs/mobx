b6d80268d51ae2820ba5c74de54906715d9881ba
Jan Plaček <11457665+urugator@users.noreply.github.com>
2021-06-11 21:09:51 +0200

    forceupdate

--- packages/mobx-react-lite/src/useObserver.ts
+++ packages/mobx-react-lite/src/useObserver.ts
@@ -1,6 +1,5 @@
 import { Reaction } from "mobx"
 import React from "react"
-
 import { printDebugValue } from "./utils/printDebugValue"
 import {
     addReactionToTrack,
@@ -8,7 +7,6 @@ import {
     recordReactionAsCommitted
 } from "./utils/reactionCleanupTracking"
 import { isUsingStaticRendering } from "./staticRendering"
-import { useForceUpdate } from "./utils/utils"
 
 function observerComponentNameFor(baseComponentName: string) {
     return `observer${baseComponentName}`
@@ -25,8 +23,9 @@ export function useObserver<T>(fn: () => T, baseComponentName: string = "observe
     }
 
     const [objectRetainedByReact] = React.useState(new ObjectToBeRetainedByReact())
-
-    const forceUpdate = useForceUpdate()
+    // Force update, see #2982
+    const [, setState] = React.useState()
+    const forceUpdate = () => setState([] as any)
 
     // StrictMode/ConcurrentMode/Suspense may mean that our component is
     // rendered and abandoned multiple times, so we need to track leaked
--- packages/mobx-react-lite/src/utils/utils.ts
+++ packages/mobx-react-lite/src/utils/utils.ts
@@ -1,17 +1,3 @@
-import { useCallback, useState } from "react"
-
-const EMPTY_ARRAY: any[] = []
-
-export function useForceUpdate() {
-    const [, setTick] = useState(0)
-
-    const update = useCallback(() => {
-        setTick(tick => tick + 1)
-    }, EMPTY_ARRAY)
-
-    return update
-}
-
 const deprecatedMessages: string[] = []
 
 export function useDeprecated(msg: string) {
