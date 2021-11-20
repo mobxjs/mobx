187836312e26384bd057215ab97d48146e063593
Remi van der Laan <rrm-remi@hotmail.com>
2021-11-21 00:51:20 +0100

    A few more test cases

--- packages/mobx/__tests__/v5/base/observables.js
+++ packages/mobx/__tests__/v5/base/observables.js
@@ -2192,24 +2192,34 @@ test("options can be provided only once", () => {
     }).toThrow(error)
 })
 
-test("observablearray.replace ", () => {
-    // two paths to check:
-    // - arrays smaller MAX_SPLICE_SIZE
-    const ar1 = observable([1])
-    const del1 = ar1.replace([2])
-    expect([2]).toEqual(ar1.toJSON())
-    expect([1]).toEqual(del1)
+test("observablearray.replace", () => {
+    // both lists are small
+    let ar = observable([1])
+    let del = ar.replace([2])
+    expect([2]).toEqual(ar.toJSON())
+    expect([1]).toEqual(del)
 
-    // - arrays larger than MAX_SPLICE_SIZE
-    //   - replacement is larger than current array
-    const ar2 = observable(new Array(MAX_SPLICE_SIZE))
-    const del2 = ar2.replace(new Array(MAX_SPLICE_SIZE + 1))
-    expect(MAX_SPLICE_SIZE + 1).toEqual(ar2.length)
-    expect(MAX_SPLICE_SIZE).toEqual(del2.length)
+    // the replacement is large
+    ar = observable([1])
+    del = ar.replace(new Array(MAX_SPLICE_SIZE))
+    expect(MAX_SPLICE_SIZE).toEqual(ar.length)
+    expect([1]).toEqual(del)
 
-    //   - replacement is smaller than current array
-    const ar3 = observable(new Array(MAX_SPLICE_SIZE + 1))
-    const del3 = ar3.replace(new Array(MAX_SPLICE_SIZE))
-    expect(MAX_SPLICE_SIZE).toEqual(ar3.length)
-    expect(MAX_SPLICE_SIZE + 1).toEqual(del3.length)
+    // the original is large
+    ar = observable(new Array(MAX_SPLICE_SIZE))
+    del = ar.replace([2])
+    expect([2]).toEqual(ar)
+    expect(MAX_SPLICE_SIZE).toEqual(del.length)
+
+    // both are large; original larger than replacement
+    ar = observable(new Array(MAX_SPLICE_SIZE + 1))
+    del = ar.replace(new Array(MAX_SPLICE_SIZE))
+    expect(MAX_SPLICE_SIZE).toEqual(ar.length)
+    expect(MAX_SPLICE_SIZE + 1).toEqual(del.length)
+
+    // both are large; replacement larger than original
+    ar = observable(new Array(MAX_SPLICE_SIZE))
+    del = ar.replace(new Array(MAX_SPLICE_SIZE + 1))
+    expect(MAX_SPLICE_SIZE + 1).toEqual(ar.length)
+    expect(MAX_SPLICE_SIZE).toEqual(del.length)
 })
