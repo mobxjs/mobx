e0ec9a6dc1fc0b22fbb4099d35e7887cd644f319
Alex Hoisl <32290337+ahoisl@users.noreply.github.com>
2021-12-18 00:53:59 +0100

    fix(eslint): Fix missing-observer rule name in recommended

--- packages/eslint-plugin-mobx/src/index.js
+++ packages/eslint-plugin-mobx/src/index.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict"
 
-const exhaustiveMakeObservable = require('./exhaustive-make-observable.js');
-const unconditionalMakeObservable = require('./unconditional-make-observable.js');
-const missingMakeObservable = require('./missing-make-observable.js');
-const missingObserver = require('./missing-observer');
-const noAnonymousObserver = require('./no-anonymous-observer.js');
+const exhaustiveMakeObservable = require("./exhaustive-make-observable.js")
+const unconditionalMakeObservable = require("./unconditional-make-observable.js")
+const missingMakeObservable = require("./missing-make-observable.js")
+const missingObserver = require("./missing-observer")
+const noAnonymousObserver = require("./no-anonymous-observer.js")
 
 module.exports = {
-  configs: {
-    recommended: {
-      plugins: ['mobx'],
-      rules: {
-        'mobx/exhaustive-make-observable': 'warn',
-        'mobx/unconditional-make-observable': 'error',
-        'mobx/missing-make-observable': 'error',
-        'mobx/no-missing-observer': 'warn',
-        'mobx/no-anonymous-observer': 'warn',
-      },
+    configs: {
+        recommended: {
+            plugins: ["mobx"],
+            rules: {
+                "mobx/exhaustive-make-observable": "warn",
+                "mobx/unconditional-make-observable": "error",
+                "mobx/missing-make-observable": "error",
+                "mobx/missing-observer": "warn",
+                "mobx/no-anonymous-observer": "warn"
+            }
+        }
     },
-  },
-  rules: {
-    'exhaustive-make-observable': exhaustiveMakeObservable,
-    'unconditional-make-observable': unconditionalMakeObservable,
-    'missing-make-observable': missingMakeObservable,
-    'missing-observer': missingObserver,
-    'no-anonymous-observer': noAnonymousObserver,
-  }
+    rules: {
+        "exhaustive-make-observable": exhaustiveMakeObservable,
+        "unconditional-make-observable": unconditionalMakeObservable,
+        "missing-make-observable": missingMakeObservable,
+        "missing-observer": missingObserver,
+        "no-anonymous-observer": noAnonymousObserver
+    }
 }
