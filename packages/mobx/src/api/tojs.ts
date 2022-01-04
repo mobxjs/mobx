bf5da6bad982dc3d955d5f27f7fea6f94b041ea7
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2022-01-04 11:54:18 +0000

    Warn in `toJS` docs about deep cloning partial observables (#3239)

--- packages/mobx/src/api/tojs.ts
+++ packages/mobx/src/api/tojs.ts
@@ -62,7 +62,10 @@ function toJSHelper(source, __alreadySeen: Map<any, any>) {
 }
 
 /**
- * Basically, a deep clone, so that no reactive property will exist anymore.
+ * Recursively converts an observable to it's non-observable native counterpart.
+ * It does NOT recurse into non-observables, these are left as they are, even if they contain observables.
+ * Computed and other non-enumerable properties are completely ignored.
+ * Complex scenarios require custom solution, eg implementing `toJSON` or using `serializr` lib.
  */
 export function toJS<T>(source: T, options?: any): T {
     if (__DEV__ && options) die("toJS no longer supports options")
