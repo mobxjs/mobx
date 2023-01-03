ebcd104e0b67f90073afd4877ab5c963f71a3550
Michel Weststrate <mweststrate@fb.com>
2023-01-03 20:18:49 +0100

    docs: updated analytics to GA4

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -121,7 +121,17 @@ myOtherFont: [
 
     // You may provide arbitrary config keys to be used as needed by your
     // template. For example, if you need your repo's URL...
-    repoUrl: "https://github.com/mobxjs/mobx"
+    repoUrl: "https://github.com/mobxjs/mobx",
+
+    plugins: [
+        [
+			"@docusaurus/plugin-google-gtag",
+			{
+				trackingID: "G-7XBWTSGJ80",
+				anonymizeIP: true
+			}
+		],
+    ]
 }
 
 module.exports = siteConfig
