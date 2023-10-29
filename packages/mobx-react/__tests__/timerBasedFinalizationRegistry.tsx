fdf9b784eadcc397c335a2f4888f319901247ceb
urugator <11457665+urugator@users.noreply.github.com>
2023-10-29 13:01:07 +0100

    add test for #3776 (#3787)

---
+++ packages/mobx-react/__tests__/timerBasedFinalizationRegistry.tsx
@@ -0,0 +1,36 @@
+import "./utils/killFinalizationRegistry"
+import { cleanup, render, act } from "@testing-library/react"
+import * as mobx from "mobx"
+import * as React from "react"
+import { observer } from "../src"
+import { clearTimers } from "mobx-react-lite"
+
+expect(globalThis.FinalizationRegistry).toBeUndefined()
+
+afterEach(cleanup)
+
+test("should unregister from FinalizationRegistry once commited #3776", async () => {
+    const o = mobx.observable({ x: 0 })
+
+    @observer
+    class TestCmp extends React.Component<any> {
+        render() {
+            return o.x
+        }
+    }
+
+    const { unmount, container } = render(<TestCmp />)
+
+    expect(container).toHaveTextContent("0")
+
+    // If not unregistered, clearTimes disposes reaction
+    clearTimers()
+
+    act(() => {
+        o.x++
+    })
+
+    expect(container).toHaveTextContent("1")
+
+    unmount()
+})
