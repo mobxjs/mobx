46df92bf3af510c27a09b77887c5cd579169c1ce
Igor Zviagintsev <inoyakaigor@ya.ru>
2024-05-17 13:04:22 +0400

    3850 - update TS to version with new Set methods; remove copied types for it

--- packages/mobx/src/global.d.ts
+++ packages/mobx/src/global.d.ts
@@ -1,18 +1,1 @@
 declare const __DEV__: boolean
-
-// This code is a copy from TS PR#57230
-// Should be removed after it merged
-interface ReadonlySetLike<T> {
-    /**
-     * Despite its name, returns an iterable of the values in the set-like.
-     */
-    keys(): Iterator<T>
-    /**
-     * @returns a boolean indicating whether an element with the specified value exists in the set-like or not.
-     */
-    has(value: T): boolean
-    /**
-     * @returns the number of (unique) elements in the set-like.
-     */
-    readonly size: number
-}
