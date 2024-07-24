638533e592f4fda7663fa351447380f9d0917d14
urugator <11457665+urugator@users.noreply.github.com>
2024-07-24 21:36:35 +0200

    Fix #3908: mobx/missing-observer rule false positive with forwardRef (#3909)

---
+++ .changeset/fifty-tools-reply.md
@@ -0,0 +1,5 @@
+---
+"eslint-plugin-mobx": patch
+---
+
+mobx/missing-observer rule false positive with forwardRef #3908
--- packages/eslint-plugin-mobx/src/missing-observer.js
+++ packages/eslint-plugin-mobx/src/missing-observer.js
@@ -1,63 +1,94 @@
-'use strict';
+"use strict"
 
 function create(context) {
-  const sourceCode = context.getSourceCode();
+    const sourceCode = context.getSourceCode()
 
-  return {
-    'FunctionDeclaration,FunctionExpression,ArrowFunctionExpression,ClassDeclaration,ClassExpression': cmp => {
-      // Already has observer
-      if (cmp.parent && cmp.parent.type === 'CallExpression' && cmp.parent.callee.name === 'observer') return;
-      let name = cmp.id?.name;
-      // If anonymous try to infer name from variable declaration      
-      if (!name && cmp.parent?.type === 'VariableDeclarator') {
-        name = cmp.parent.id.name;
-      }
-      if (cmp.type.startsWith('Class')) {
-        // Must extend Component or React.Component
-        const { superClass } = cmp;
-        if (!superClass) return;
-        const superClassText = sourceCode.getText(superClass);
-        if (superClassText !== 'Component' && superClassText !== 'React.Component') return;
-      } else {
-        // Name must start with uppercase letter
-        if (!name?.charAt(0).match(/^[A-Z]$/)) return;
-      }
+    return {
+        "FunctionDeclaration,FunctionExpression,ArrowFunctionExpression,ClassDeclaration,ClassExpression":
+            cmp => {
+                if (
+                    cmp.parent &&
+                    cmp.parent.type === "CallExpression" &&
+                    cmp.parent.callee.name === "observer"
+                ) {
+                    // observer(...)
+                    return
+                }
+                let forwardRef =
+                    cmp.parent &&
+                    cmp.parent.type === "CallExpression" &&
+                    cmp.parent.callee.name === "forwardRef"
+                        ? cmp.parent
+                        : undefined
+                if (
+                    forwardRef &&
+                    forwardRef.parent &&
+                    forwardRef.parent.type === "CallExpression" &&
+                    forwardRef.parent.callee.name === "observer"
+                ) {
+                    // forwardRef(observer(...))
+                    return
+                }
 
-      const fix = fixer => {
-        return [
-          fixer.insertTextBefore(
-            sourceCode.getFirstToken(cmp),
-            (name && cmp.type.endsWith('Declaration') ? `const ${name} = ` : '') + 'observer(',
-          ),
-          fixer.insertTextAfter(
-            sourceCode.getLastToken(cmp),
-            ')',
-          ),
-        ]
-      }
-      context.report({
-        node: cmp,
-        messageId: 'missingObserver',
-        data: {
-          name: name || '<anonymous>',
-        },
-        fix,
-      })
-    },
-  };
+                const cmpOrForwardRef = forwardRef || cmp
+                let name = cmp.id?.name
+                // If anonymous try to infer name from variable declaration
+                if (!name && cmpOrForwardRef.parent?.type === "VariableDeclarator") {
+                    name = cmpOrForwardRef.parent.id.name
+                }
+                if (cmp.type.startsWith("Class")) {
+                    // Must extend Component or React.Component
+                    const { superClass } = cmp
+                    if (!superClass) {
+                        // not a component
+                        return
+                    }
+                    const superClassText = sourceCode.getText(superClass)
+                    if (superClassText !== "Component" && superClassText !== "React.Component") {
+                        // not a component
+                        return
+                    }
+                } else {
+                    // Name must start with uppercase letter
+                    if (!name?.charAt(0).match(/^[A-Z]$/)) {
+                        // not a component
+                        return
+                    }
+                }
+
+                const fix = fixer => {
+                    return [
+                        fixer.insertTextBefore(
+                            sourceCode.getFirstToken(cmpOrForwardRef),
+                            (name && cmp.type.endsWith("Declaration") ? `const ${name} = ` : "") +
+                                "observer("
+                        ),
+                        fixer.insertTextAfter(sourceCode.getLastToken(cmpOrForwardRef), ")")
+                    ]
+                }
+                context.report({
+                    node: cmp,
+                    messageId: "missingObserver",
+                    data: {
+                        name: name || "<anonymous>"
+                    },
+                    fix
+                })
+            }
+    }
 }
 
 module.exports = {
-  meta: {
-    type: 'problem',
-    fixable: 'code',
-    docs: {
-      description: 'prevents missing `observer` on react component',
-      recommended: true,
+    meta: {
+        type: "problem",
+        fixable: "code",
+        docs: {
+            description: "prevents missing `observer` on react component",
+            recommended: true
+        },
+        messages: {
+            missingObserver: "Component `{{ name }}` is missing `observer`."
+        }
     },
-    messages: {
-      missingObserver: "Component `{{ name }}` is missing `observer`.",
-    },
-  },
-  create,
-};
+    create
+}
