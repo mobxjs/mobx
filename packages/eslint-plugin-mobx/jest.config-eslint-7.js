981917e4b41e5c80f8d0d9e1b7b9fd4cbdbeadb8
Sergey Kozlov <dartess@mail.ru>
2024-10-19 21:29:24 +0400

    add distinct displayName for each eslint-plugin-mobx jest config

--- packages/eslint-plugin-mobx/jest.config-eslint-7.js
+++ packages/eslint-plugin-mobx/jest.config-eslint-7.js
@@ -1,6 +1,7 @@
 const buildConfig = require("../../jest.base.config")
 
 module.exports = buildConfig(__dirname, {
+    displayName: 'eslint-plugin-mobx with eslint@7',
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
     testRegex: "__tests__/[^/]+\\.(t|j)sx?$",
     globals: {
