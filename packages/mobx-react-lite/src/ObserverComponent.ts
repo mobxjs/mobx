90a22728e8e520d8a9cc967e6ea1f7ebbe64ff9e
Jordan Thomson <jordant@canva.com>
2024-12-11 10:00:37 +1100

    render time check

--- packages/mobx-react-lite/src/ObserverComponent.ts
+++ packages/mobx-react-lite/src/ObserverComponent.ts
@@ -6,6 +6,11 @@ interface IObserverProps {
 }
 
 function ObserverComponent({ children, render }: IObserverProps) {
+    if (children && render) {
+        console.error(
+            "MobX Observer: Do not use children and render in the same time in `Observer`"
+        )
+    }
     const component = children || render
     if (typeof component !== "function") {
         return null
