d54fb911821d5307a297f069b8400b747686e235
Sebastian Malton <sebastian@malton.name>
2026-01-21 11:58:27 -0500

    fix(mobx-react-lite): Ensure onBecomeUnobserved is properly called immediately on unmount of components that did suspend

--- packages/mobx-react-lite/__tests__/strictAndConcurrentMode.test.tsx
+++ packages/mobx-react-lite/__tests__/strictAndConcurrentMode.test.tsx
@@ -3,14 +3,15 @@ import mockConsole from "jest-mock-console"
 import * as mobx from "mobx"
 import * as React from "react"
 
-import { useObserver } from "../src/useObserver"
+import { ErrorBoundary } from "./utils/error-bondary"
+import { observer } from "../src/observer"
 
 afterEach(cleanup)
 
 test("uncommitted observing components should not attempt state changes", () => {
     const store = mobx.observable({ count: 0 })
 
-    const TestComponent = () => useObserver(() => <div>{store.count}</div>)
+    const TestComponent = observer(() => <div>{store.count}</div>)
 
     // Render our observing component wrapped in StrictMode
     const rendering = render(
@@ -44,15 +45,14 @@ test("uncommitted observing components should not attempt state changes", () => 
 test(`observable changes before first commit are not lost`, async () => {
     const store = mobx.observable({ value: "initial" })
 
-    const TestComponent = () =>
-        useObserver(() => {
-            const res = <div>{store.value}</div>
-            // Change our observable. This is happening between the initial render of
-            // our component and its initial commit, so it isn't fully mounted yet.
-            // We want to ensure that the change isn't lost.
-            store.value = "changed"
-            return res
-        })
+    const TestComponent = observer(() => {
+        const res = <div>{store.value}</div>
+        // Change our observable. This is happening between the initial render of
+        // our component and its initial commit, so it isn't fully mounted yet.
+        // We want to ensure that the change isn't lost.
+        store.value = "changed"
+        return res
+    })
 
     const rootNode = document.createElement("div")
     document.body.appendChild(rootNode)
@@ -64,4 +64,151 @@ test(`observable changes before first commit are not lost`, async () => {
     )
 
     expect(rendering.baseElement.textContent).toBe("changed")
+})
+
+test("suspended components should not leak observations", async () => {
+    const o = mobx.observable({ x: 0, promise: null as Promise<void> | null })
+    const Cmp = observer(() => {
+        o.x as any // establish dependency
+
+        if (o.promise) {
+            throw o.promise
+        }
+
+        return <>{o.x}</>
+    })
+
+    const observed = jest.fn()
+    const unobserved = jest.fn()
+    mobx.onBecomeObserved(o, "x", observed)
+    mobx.onBecomeUnobserved(o, "x", unobserved)
+
+    jest.useFakeTimers()
+    const { container, unmount } = render(
+        <React.Suspense fallback={"loading..."}>
+            <Cmp />
+        </React.Suspense>
+    )
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    expect(container).toHaveTextContent("0")
+    expect(observed).toBeCalledTimes(1)
+
+    let resolve: () => void
+    act(() => {
+        o.promise = new Promise(r => (resolve = r))
+    })
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    expect(container).toHaveTextContent("loading...")
+    expect(observed).toBeCalledTimes(1)
+    expect(unobserved).toBeCalledTimes(0)
+
+    act(() => {
+        o.promise = null
+        resolve!()
+    })
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    expect(container).toHaveTextContent(o.x + "")
+
+    // ensure that we using same reaction and component state
+    expect(observed).toBeCalledTimes(1)
+    expect(unobserved).toBeCalledTimes(0)
+
+    act(() => {
+        o.x++
+    })
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    expect(container).toHaveTextContent(o.x + "")
+
+    unmount()
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    expect(observed).toBeCalledTimes(1)
+    expect(unobserved).toBeCalledTimes(1)
+    jest.useRealTimers()
+})
+
+test("uncommitted components should not leak observations", async () => {
+    const store = mobx.observable({ count1: 0, count2: 0 })
+
+    // Track whether counts are observed
+    let count1IsObserved = false
+    let count2IsObserved = false
+    mobx.onBecomeObserved(store, "count1", () => (count1IsObserved = true))
+    mobx.onBecomeUnobserved(store, "count1", () => (count1IsObserved = false))
+    mobx.onBecomeObserved(store, "count2", () => (count2IsObserved = true))
+    mobx.onBecomeUnobserved(store, "count2", () => (count2IsObserved = false))
+
+    const TestComponent1 = observer(() => <div>{store.count1}</div>)
+    const TestComponent2 = observer(() => <div>{store.count2}</div>)
+
+    jest.useFakeTimers()
+    // Render, then remove only #2
+    const rendering = render(
+        <React.StrictMode>
+            <TestComponent1 />
+            <TestComponent2 />
+        </React.StrictMode>
+    )
+    rendering.rerender(
+        <React.StrictMode>
+            <TestComponent1 />
+        </React.StrictMode>
+    )
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+    // count1 should still be being observed by Component1,
+    // but count2 should have had its reaction cleaned up.
+    expect(count1IsObserved).toBeTruthy()
+    expect(count2IsObserved).toBeFalsy()
+
+    jest.useRealTimers()
+})
+
+test("abandoned components should not leak observations", async () => {
+    const store = mobx.observable({ count: 0 })
+    const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {})
+
+    let countIsObserved = false
+    mobx.onBecomeObserved(store, "count", () => (countIsObserved = true))
+    mobx.onBecomeUnobserved(store, "count", () => (countIsObserved = false))
+
+    const TestComponent = observer(() => {
+        store.count // establish dependency
+        throw new Error("not rendered")
+    })
+
+    jest.useFakeTimers()
+
+    render(
+        <ErrorBoundary fallback="error">
+            <TestComponent />
+        </ErrorBoundary>
+    )
+
+    expect(countIsObserved).toBeTruthy()
+
+    await act(async () => {
+        jest.runAllTimers()
+    })
+
+    expect(countIsObserved).toBeFalsy()
+
+    jest.useRealTimers()
+    consoleErrorSpy.mockRestore()
 })
